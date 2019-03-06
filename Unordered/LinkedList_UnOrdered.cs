822de7611db38006480307af8ce935eb5e61bffe
Krishnak Kulkarni <krishnakkulkarni1@gmail.com>
2019-03-06 06:12:01 -0800

    DataStructureProgram

---
+++ Unordered/LinkedList_UnOrdered.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LinkedList_UnOrdered.cs" company="Bridgelabz">
+//     Company @ 2019 </copyright>
+// <creator name = "Krishna Kulkarni" />
+//-----------------------------------------------------------------------
+namespace DataStructurePrograms.Unordered
+{
+ using System;
+ using System.Collections.Generic;
+
+    /// <summary>
+    /// class for Unordered List
+    /// </summary>
+    public class LinkedList_UnOrdered
+    {
+        /// <summary>
+        /// Unordered LinkedList
+        /// </summary>
+        public static void UnOrderedList()
+        {
+            try
+            {
+                string path = string.Empty;
+                SinglyLinkedList<string> singlyLinkedList = new SinglyLinkedList<string>();
+
+                string dataFromFile = System.IO.File.ReadAllText("C:/Users/admin/source/repos/DataStructurePrograms/text.txt");
+                string[] splitArray = dataFromFile.Split();
+                foreach (string text in splitArray)
+                {
+                    Console.Write(text + "\t");
+                }
+
+                Console.WriteLine();
+                
+                foreach (string word in splitArray)
+                {
+                    singlyLinkedList.Add(word.Trim());
+                }
+
+                Console.WriteLine("Enter the word you want to search");
+                string wordToSearch = Console.ReadLine();
+
+                if (singlyLinkedList.Contain(wordToSearch))
+                {
+                    if (singlyLinkedList.Delete(wordToSearch))
+                    {
+                        Console.WriteLine("Word " + wordToSearch + " has been removed ");
+                    }
+                }
+                else
+                {
+                    singlyLinkedList.Add(wordToSearch);
+                    Console.WriteLine("The word " + wordToSearch + " not found , Added to linked list and written to file");
+                }
+
+                singlyLinkedList.WriteToFile(path);
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Utility.cs" company="Bridgelabz">
+//     Company @ 2019 </copyright>
+// <creator name = "Krishna Kulkarni" />
+//-----------------------------------------------------------------------
+namespace DataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+   
+    /// <summary>
+    /// Entry point of the Main class
+    /// </summary>
+    public class Utility
+    {
+        /// <summary>
+        /// Defines the entry point of the application
+        /// </summary>
+        /// <exception cref="Exception">handles and throws Exception</exception>
+        public static void Main()
+        {
+            try
+            {
+                int option = -1; ////option for choosing a number from Utility.
+                bool flag = true; ////flag for keep looping untill a valid option from the existing Utility Class
+                while (flag)
+                {
+                    Console.WriteLine("Select the program from below .Enter 0 to exit");
+                    Console.WriteLine("1. Unordered LinkedList");
+
+                    try
+                    {
+                        option = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid Command");
+                        continue;
+                    }
+
+                    switch (option)
+                    {
+                        case 0:
+                            {
+                                flag = false;
+                                break;
+                            }
+
+                        case 1:
+                            {
+                                DataStructurePrograms.Unordered.LinkedList_UnOrdered.UnOrderedList();
+                                break;
+                            }
+
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option");
+                                break;
+                            }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
